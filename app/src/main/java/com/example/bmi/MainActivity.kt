4ca9cc01463db25cb3656c764ef2ab8b5e1b8694
TARUMT <joanneoye-wm21@student.tarc.edu.my>
2024-03-04 09:45:46 +0800

    hi

--- app/src/main/java/com/example/bmi/MainActivity.kt
+++ app/src/main/java/com/example/bmi/MainActivity.kt
@@ -1,61 +1,43 @@
 package com.example.bmi
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
-import com.example.bmi.databinding.ActivityMainBinding
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        var tfWeight : TextView = findViewById(R.id.weight)
+        var tfHeight : TextView = findViewById(R.id.tfHeight)
+        var btn : Button = findViewById(R.id.btnSubmit)
+        var imgResult : ImageView = findViewById(R.id.img)
 
-        setSupportActionBar(binding.toolbar)
+        btn.setOnClickListener(){
+            var weight : Double = tfWeight.text.toString().toDouble()
+            var height : Double = tfHeight.text.toString().toDouble()
+            var bmi = weight/height * height
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+            if(bmi < 18.5){
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAnchorView(R.id.fab)
-                .setAction("Action", null).show()
+                imgResult.setImageResource(R.drawable.under)
+
+            }
+            else if (bmi >= 18.5 && bmi <= 25.9){
+            imgResult.setImageResource(R.drawable.normal)
+    }else
+            {
+            imgResult.setImageResource(R.drawable.over)
+            }
+            }
         }
-    }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
-    }
 }
